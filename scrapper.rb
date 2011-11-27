1463efc853d5c2cc234004ada7796059b972d52d
Joshua Kelly <jdjkelly@gmail.com>
2011-11-26 21:07:31 -0500

    working model

--- scrapper.rb
+++ scrapper.rb
@@ -1,29 +1,19 @@
-require 'rubygems'
+#require 'rubygems'
 require 'net/http'
+require 'open-uri'
 require 'json'
+require 'mp3info'
+require 'fileutils'
 
-# Create new connection object
+p "Starting this bitch up..."
+
+# Create new connection object and grab the AUTH cookie. This is how Hypem validates the keys it generates.
 conn = Net::HTTP.new('hypem.com', 80)
-
-# Get the response on initial request to grab the cookie!
-response = conn.get('/popular')
+response = conn.get('/')
 cookie = response.response['set-cookie']
-re = /AUTH=\S{70}/
-auth = cookie.match re
 
 # Headers need to be in a hash.
-headers = {
-	'Host' => 'hypem.com',
-	'Connection' => 'keep-alive',
-	'X-Requested-With' => 'XMLHttpRequest',
-	'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2',
-	'Accept' => 'application/json, text/javascript, */*; q=0.01',
-	'Referer' => 'http://hypem.com/',
-	'Accept-Encoding' => 'gzip,deflate,sdch',
-	'Accept-Language' => 'en-US,en;q=0.8',
-	'Accept-Charset' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-	'Cookie' => auth[0].to_s
-	}
+headers = {	'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; rv:1.9) Gecko/20100101 Firefox/4.0', 'Cookie' => cookie }
 
 # Get the data from hypem
 response = conn.get('/popular', headers)
@@ -37,9 +27,9 @@ regex_input.scan(/\w{32}/) { |match|
 
 # Now we have to get the song ids. In the future, maybe I should scrape them from the same source as the keys - when I get better at ruby regex's.
 def getids 
-	http = "http://hypem.com/playlist/popular/3day/json/1/data.js"
-	req = Net::HTTP.get_response(URI.parse(http))
-	response = req.body
+	feed = "http://hypem.com/playlist/popular/3day/json/1/data.js"
+	request = Net::HTTP.get_response(URI.parse(feed))
+	response = request.body
 	json_parse = JSON.parse(response, {'symbolize_names' => true})
 	return json_parse
 end
@@ -48,15 +38,36 @@ end
 ids = Array.new
 getids.each_with_index do |item, index|
 	# The first part of the object is a version identifier, so we'll skip it.
-	if index > 1
+	if index > 0
 		ids << item[1]["mediaid"]
 	end
 end
 
-for i in (1..1)
-	url = "/serve/source/" + ids[i] + "/" + keys[i] + "?_=" + Time.now.to_i.to_s
-	response = conn.get(url, headers)
-	header = response.header
-	p header
+# This is where the magic happens
+for i in (0..9)
+	url = "/serve/source/" + ids[i] + "/" + keys[i]
+	request = conn.get(url, headers)
+	response = request.body
+	json_obj = JSON.parse(response, {'symbolize_names' => true})
+	mp3_url = json_obj["url"]
+  	
+  	# Save File
+	filename = i.to_s + ".mp3"
+	writeOut = open(filename, "wb")
+	  	writeOut.write(open(mp3_url).read)
+	writeOut.close
+
+	# Rewrite file name
+  	Mp3Info.open(filename) do |mp3|
+    	title = mp3.tag.title 
+    	artist = mp3.tag.artist
+    	p "Downloading: " + title + " - " + artist + "..."
+    	new_filename = '0' + (i + 1).to_s + ' ' + artist + ' - ' + title + '.mp3'
+    	File.rename(filename, new_filename ) 
+    	p "Done."
+	end
+
+
 end
+
 
