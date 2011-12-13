b73282e490770207cc39b2ba7c868638798f7959
Joshua Kelly <jdjkelly@gmail.com>
2011-12-12 22:11:10 -0500

    Refactored main app, introducing classes, and added design file

--- app.rb
+++ app.rb
@@ -3,50 +3,44 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 
-# Grabs the json feeds 
-def getpopular(page)
-      feed = "http://hypem.com/playlist/popular/3day/json/" + page.to_s + "/data.js"
-      #feed = "http://localhost:4567/json.js"
-      request = Net::HTTP.get_response(URI.parse(feed))
-      response = request.body
-      json_parse = JSON.parse(response, {'symbolize_names' => true})
-      return json_parse
+# Grabs the json feeds
+class Popularblogs
+      def getpopular
+            feed = "http://hypem.com/playlist/popular/3day/json/1/data.js"
+            request = Net::HTTP.get_response(URI.parse(feed))
+            response = request.body
+            json_parse = JSON.parse(response, {'symbolize_names' => true})
+      end
 end
 
+get "/" do 
 
-get '/' do 
-      ids = Hash.new
-            ids2 = Hash.new
-            for i in (1..5)
-                  getpopular(i).each_with_index do |item, index|
-                        if index > 0
-                              sitename = item[1]["sitename"]
-                              if ids.has_key?(sitename)
-                                    old_value = ids[sitename]
-                                    ids2[sitename] = old_value + 1
-                                    ids.merge!(ids2)
-                              else      
-                                    ids[sitename] = 1
-                              end
-                        end
-                  end
-            end
-            @sorted = ids.sort_by {|k,v| v}.reverse
+      # form the new request
+      request  = Popularblogs.new.getpopular
+      itemindex = request.length - 1
+
+      # grab the parts from the json feed and stick them in a hash
+      sitenames = Hash.new
+      sitecounts = Hash.new
+
+      # loop through the request, and make a new hash of the sitenames and sitevalues
+      counter = 0
+      (1..itemindex).each do
+            sitename = request[counter.to_s]["sitename"]
+            if sitenames.has_key?(sitename)
+                  sitecount = sitenames[sitename]
+                  sitecounts[sitename] = sitecount + counter + 1
+                  sitenames.merge!(sitecounts)
+            else
+                  sitenames[sitename] = counter + 1
+            counter += 1
+      end
+
+      # sort sitenames hash by value
+      # @sorted = sitenames.sort_by {|k,v| v}
+
+
+
+
       erb :matcher
-end
-
-get '/genres' do 
-      "Coming soon"
-end
-
-get '/artists' do 
-      "Coming soon"
-end
-
-get '/tracks' do 
-      "Coming soon"
-end
-
-get '/genres' do 
-      "Coming soon"
 end
